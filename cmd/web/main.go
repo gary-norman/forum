0d040db1c21d5c949d3291a930b06f1481963647
Gary Norman <gary.norman.th@gmail.com>
2024-09-21 21:52:54 +0100

    fix: imported sqlite3 module

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "./forum_database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(db)
+
 	srv := http.Server{
 		Addr:    ":8989",
 		Handler: routes(),
 	}
 
 	log.Printf("Listening on %v", srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		return
 	}

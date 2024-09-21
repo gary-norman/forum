ee49a70ad4c3fbfea9e396ea522cb7491ef31757
Gary Norman <gary.norman.th@gmail.com>
2024-09-21 19:38:01 +0100

    fix: created template and passed it to homepage

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func getHome(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("home"))
+	t, err := template.ParseFiles("./assets/templates/index.html")
+	if err != nil {
+		//TODO log error
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		//TODO log error
+		return
+	}
 }

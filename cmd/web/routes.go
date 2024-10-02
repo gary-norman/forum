23a5810c27f6e4c692d2b7cac069b83768c72855
Gary Norman <gary.norman.th@gmail.com>
2024-10-02 04:09:13 +0100

    feat: added styling to the home page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,8 +6,19 @@ import (
 
 func (app *app) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", app.getHome)
-	mux.HandleFunc("GET /posts/create", app.createPost)
-	mux.HandleFunc("POST /posts/create", app.storePost)
+	fileServer := http.FileServer(http.Dir("./assets/css"))
+	mux.Handle("/assets/css/", http.StripPrefix("/assets/css", fileServer))
+	mux.HandleFunc("/", app.getHome)
+	// Use a single route for /posts/create and distinguish based on HTTP method
+	mux.HandleFunc("/posts/create", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			app.createPost(w, r) // Handle GET requests
+		case http.MethodPost:
+			app.storePost(w, r) // Handle POST requests
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 	return mux
 }

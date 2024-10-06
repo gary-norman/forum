289acbccc4f30da4c0366b6e81396e984a06257d
Gary Norman <gary.norman.th@gmail.com>
2024-10-07 00:50:29 +0100

    feat: header is completed (semi-responsive), routes added for all asset directories

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,8 +6,16 @@ import (
 
 func (app *app) routes() http.Handler {
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./assets/css"))
-	mux.Handle("/assets/css/", http.StripPrefix("/assets/css", fileServer))
+	cssServer := http.FileServer(http.Dir("./assets/css"))
+	mux.Handle("/assets/css/", http.StripPrefix("/assets/css", cssServer))
+	jsServer := http.FileServer(http.Dir("./assets/js"))
+	mux.Handle("/assets/js/", http.StripPrefix("/assets/js", jsServer))
+	iconServer := http.FileServer(http.Dir("./assets/icons"))
+	mux.Handle("/assets/icons/", http.StripPrefix("/assets/icons", iconServer))
+	imageServer := http.FileServer(http.Dir("./assets/images"))
+	mux.Handle("/assets/images/", http.StripPrefix("/assets/images", imageServer))
+	fontServer := http.FileServer(http.Dir("./assets/images"))
+	mux.Handle("/assets/fonts/", http.StripPrefix("/assets/fonts", fontServer))
 	mux.HandleFunc("/", app.getHome)
 	// Use a single route for /posts/create and distinguish based on HTTP method
 	mux.HandleFunc("/posts/create", func(w http.ResponseWriter, r *http.Request) {

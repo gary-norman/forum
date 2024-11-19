f076f1a54dd4899262a00ad9745dfb6d075f73d7
Gary Norman <gary.norman.th@gmail.com>
2024-11-19 00:21:20 +0000

    feat: darklight button animation working correctly, entire div checks the checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // variables
 const switchDl = document.getElementById('switch-dl');
-
+const darkSwitch = document.getElementById('sidebar-option-darkmode');
 // functions
 function toggleColorScheme() {
     // Get the current color scheme
@@ -12,6 +12,12 @@ function toggleColorScheme() {
     // Set the new color scheme
     document.documentElement.setAttribute('color-scheme', newScheme);
 }
+function toggleDarkMode() {
+    const checkbox = document.getElementById("darkmode-checkbox");
+    checkbox.checked = !checkbox.checked;
+    console.log("toggle dark mode")
+}
 
 // event listeners
-switchDl.addEventListener('click', toggleColorScheme);
+// switchDl.addEventListener('click', toggleColorScheme);
+darkSwitch.addEventListener('click', toggleDarkMode);

49dcc838f3765eb561acb1ea6cbd9e2797d41598
kornal <kamilornal@gmail.com>
2024-11-23 14:47:10 +0000

    maint: added images  posting to database

---
+++ internal/sqlite/images.go
@@ -0,0 +1,48 @@
+package sqlite
+
+import (
+	"database/sql"
+	"github.com/gary-norman/forum/internal/models"
+	"log"
+)
+
+type ImageModel struct {
+	DB *sql.DB
+}
+
+func (m *ImageModel) Insert(authotID, postID int) error {
+	stmt := "INSERT INTO Images (Created, AuthorID, PostID) VALUES (DateTime('now'), ?, ?)"
+	_, err := m.DB.Exec(stmt, authotID, postID)
+	return err
+}
+
+func (m *ImageModel) All() ([]models.Image, error) {
+	ErrorMsgs := models.CreateErrorMessages()
+	stmt := "SELECT ID, Created, AuthorID, PostID FROM Images ORDER BY ID DESC"
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if closeErr := rows.Close(); closeErr != nil {
+			log.Printf(ErrorMsgs.Close, "rows", "All")
+		}
+	}()
+
+	var Images []models.Image
+	for rows.Next() {
+		p := models.Image{}
+		err = rows.Scan(&p.ID, &p.Created, &p.AuthorID, &p.PostID)
+		if err != nil {
+			return nil, err
+		}
+		Images = append(Images, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return Images, nil
+}

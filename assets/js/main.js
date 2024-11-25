aff42655d810d5bdd4dbb4a40712e6e675bf08e0
Gary Norman <gary.norman.th@gmail.com>
2024-11-25 13:53:18 +0000

    feat: userpage feeds entry/exit working correctly

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const actButtonsAll = actButtonContainer.querySelectorAll('button')
 // activity feeds
 const activityFeeds = document.querySelector('#activity-feeds')
 const activityFeedsAll = activityFeeds.querySelectorAll('[id^="activity"]')
+const activityFeedsContentAll = activityFeeds.querySelectorAll('[id^="activity-feed-"]')
 
 // functions
 function toggleColorScheme() {
@@ -23,22 +24,28 @@ function toggleDarkMode() {
     console.log("toggle dark mode")
 }
 
-function toggleFeed(targetFeed, targetButton) {
+function toggleFeed(targetFeed, targetFeedContent, targetButton) {
     actButtonsAll.forEach( button => button.classList.remove('btn-active') );
     activityFeedsAll.forEach(feed => {
         feed.classList.remove('collapsible-expanded');
         feed.classList.add('collapsible-collapsed');
     });
+    activityFeedsContentAll.forEach(feed => {
+        feed.classList.remove('collapsible-expanded');
+        feed.classList.add('collapsible-collapsed');
+    });
     setTimeout(() => {
-        targetFeed.classList.remove('collapsible-collapsed'); }, 500);
+        targetFeed.classList.remove('collapsible-collapsed');
+        targetFeedContent.classList.remove('collapsible-collapsed');
         targetFeed.classList.add('collapsible-expanded');
-        targetButton.classList.toggle('btn-active');
+        targetFeedContent.classList.add('collapsible-expanded');
+        targetButton.classList.toggle('btn-active'); }, 500);
 }
 
 // event listeners
 // switchDl.addEventListener('click', toggleColorScheme);
 darkSwitch.addEventListener('click', toggleDarkMode);
 actButtonsAll.forEach( button => button.addEventListener('click', (e) => {
-    toggleFeed(document.getElementById("activity-" + e.target.id), e.target);
+    toggleFeed(document.getElementById("activity-" + e.target.id),document.getElementById("activity-feed-" + e.target.id),  e.target);
     console.log('activity-' + e.target.id);
 }) );

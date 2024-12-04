c45bf531bd555171c79ba0584f8fb19b26c69dcb
Gary Norman <gary.norman.th@gmail.com>
2024-12-04 00:47:54 +0000

    feat: welcome page login and register forms hiding and showing correctly. button span paddings corrected

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -4,6 +4,8 @@ const btnGuest = document.getElementById("btn_guest");
 const door = document.getElementById("access-door");
 const doorShadow = document.getElementById("access-door-shadow");
 const shadowClip = document.getElementById("shadow-clip");
+const formLogin = document.querySelector('#form-login')
+const formRegister = document.querySelector('#form-register')
 let login = false;
 let register = false;
 let guest = false;
@@ -11,10 +13,9 @@ let guest = false;
 btnLogin.addEventListener("click", () => {
   if (!login && !register) {
     console.log("!l !r a", login);
+    formLogin.classList.remove('display-off');
     door.classList.remove("grid-welcome-access-door-close");
     door.classList.add("grid-welcome-access-door-open");
-    // shadowClip.classList.remove("grid-welcome-access-door-close");
-    // shadowClip.classList.add("grid-welcome-access-door-open");
     doorShadow.classList.remove("grid-welcome-access-door-push-close");
     doorShadow.classList.add("grid-welcome-access-door-push-open");
     btnLogin.classList.toggle("button-3d-pressed");
@@ -24,10 +25,11 @@ btnLogin.addEventListener("click", () => {
     console.log("l !r a", login);
     door.classList.remove("grid-welcome-access-door-open");
     door.classList.add("grid-welcome-access-door-close");
-    // shadowClip.classList.remove("grid-welcome-access-door-open");
-    // shadowClip.classList.add("grid-welcome-access-door-close");
     doorShadow.classList.remove("grid-welcome-access-door-push-open");
     doorShadow.classList.add("grid-welcome-access-door-push-close");
+    setTimeout(() => {
+      door.classList.remove("grid-welcome-access-door-close");
+      formLogin.classList.add('display-off'); }, 500);
     btnLogin.classList.toggle("button-3d-pressed");
     login = false;
     console.log("l !r b", login);
@@ -35,15 +37,15 @@ btnLogin.addEventListener("click", () => {
     console.log("!l r a", login);
     door.classList.remove("grid-welcome-access-door-open");
     door.classList.add("grid-welcome-access-door-close");
-    // shadowClip.classList.remove("grid-welcome-access-door-open");
-    // shadowClip.classList.add("grid-welcome-access-door-close");
     doorShadow.classList.remove("grid-welcome-access-door-push-open");
     doorShadow.classList.add("grid-welcome-access-door-push-close");
-    setTimeout(() => { door.classList.remove("grid-welcome-access-door-close"); }, 500);
+    setTimeout(() => {
+      door.classList.remove("grid-welcome-access-door-close");
+      formLogin.classList.remove('display-off'); }, 500);
+    setTimeout(() => {
+    formRegister.classList.add('display-off'); }, 500);
     doorShadow.classList.remove("grid-welcome-access-door-push-close");
-    // shadowClip.classList.remove("grid-welcome-access-door-close");
     door.classList.add("grid-welcome-access-door-open");
-    // shadowClip.classList.add("grid-welcome-access-door-open");
     doorShadow.classList.add("grid-welcome-access-door-push-open");
     btnLogin.classList.toggle("button-3d-pressed");
     btnRegister.classList.toggle("button-3d-pressed");
@@ -58,6 +60,7 @@ btnLogin.addEventListener("click", () => {
 btnRegister.addEventListener("click", () => {
   if (!login && !register) {
     console.log("!l !r a", login);
+    formRegister.classList.remove('display-off');
     door.classList.remove("grid-welcome-access-door-close");
     door.classList.add("grid-welcome-access-door-open");
     doorShadow.classList.remove("grid-welcome-access-door-push-close");
@@ -69,6 +72,10 @@ btnRegister.addEventListener("click", () => {
     console.log("!l r a", login);
     door.classList.remove("grid-welcome-access-door-open");
     door.classList.add("grid-welcome-access-door-close");
+    setTimeout(() => {
+      door.classList.remove("grid-welcome-access-door-close");
+      formRegister.classList.add('display-off'); }, 500);
+    formLogin.classList.add('display-off');
     doorShadow.classList.remove("grid-welcome-access-door-push-open");
     doorShadow.classList.add("grid-welcome-access-door-push-close");
     btnRegister.classList.toggle("button-3d-pressed");
@@ -80,7 +87,11 @@ btnRegister.addEventListener("click", () => {
     doorShadow.classList.remove("grid-welcome-access-door-push-open");
     door.classList.add("grid-welcome-access-door-close");
     doorShadow.classList.add("grid-welcome-access-door-push-close");
-    setTimeout(() => { door.classList.remove("grid-welcome-access-door-close"); }, 500);
+    setTimeout(() => {
+      door.classList.remove("grid-welcome-access-door-close");
+      formRegister.classList.remove('display-off'); }, 500);
+    setTimeout(() => {
+    formLogin.classList.add('display-off'); }, 500);
     doorShadow.classList.remove("grid-welcome-access-door-push-close");
     door.classList.add("grid-welcome-access-door-open");
     doorShadow.classList.add("grid-welcome-access-door-push-open");

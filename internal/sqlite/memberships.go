e98c0ff575635ebab438a43419d50ff3be78c957
Gary Norman <gary.norman.th@gmail.com>
2025-01-29 03:28:55 +0000

    maint: overhauled app declaration, created functions to get joined and owned channels

--- internal/sqlite/memberships.go
+++ internal/sqlite/memberships.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gary-norman/forum/internal/models"
 	"log"
 )
@@ -14,6 +15,33 @@ func (m *MembershipModel) Insert(userID, channelID int) error {
 	stmt := "INSERT INTO Memberships (UserID, ChannelID, Created) VALUES (?, ?, DateTime('now'))"
 	_, err := m.DB.Exec(stmt, userID, channelID)
 	return err
+}
+
+func (m *MembershipModel) UserMemberships(userID int) ([]models.Membership, error) {
+	stmt := "SELECT ID, UserID, ChannelID, Created FROM Memberships WHERE ID = ?"
+	rows, queryErr := m.DB.Query(stmt, userID)
+	if queryErr != nil {
+		return nil, queryErr
+	}
+	defer func() {
+		if closeErr := rows.Close(); closeErr != nil {
+			log.Printf(ErrorMsgs().Close, rows, "UserMemberships", closeErr)
+		}
+	}()
+	var memberships []models.Membership
+	for rows.Next() {
+		p := models.Membership{}
+		scanErr := rows.Scan(&p.ID, &p.UserID, &p.ChannelID, &p.Created)
+		if scanErr != nil {
+			return nil, scanErr
+		}
+		memberships = append(memberships, p)
+	}
+	if rowsErr := rows.Err(); rowsErr != nil {
+		return nil, rowsErr
+	}
+	fmt.Printf(ErrorMsgs().KeyValuePair, "Channels", len(memberships))
+	return memberships, nil
 }
 
 func (m *MembershipModel) All() ([]models.Membership, error) {

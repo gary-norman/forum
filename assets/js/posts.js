ce2d03e3c86394dc7b27c57dacecd3399dab4642
Gary Norman <gary.norman.th@gmail.com>
2025-03-26 15:23:04 +0000

    maint: links from post cards are now working correctly (linking to corect address)

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -5,21 +5,27 @@ const linkPostChannels = document.querySelectorAll('[id^="link-post-channel"]');
 // TODO: change these to forEach
 
 document.addEventListener("DOMContentLoaded", () => {
-  linkPostPosts.addEventListener("click", () => {
-    const postId = linkPostPost.dataset.postid;
-    const postUrl = `/posts/${postId}`;
-    window.location.href = postUrl;
+  linkPostPosts.forEach((post) => {
+    post.addEventListener("click", () => {
+      const postId = post.dataset.postid;
+      const postUrl = `/posts/${postId}`;
+      window.location.href = postUrl;
+    });
   });
 
-  linkPostAuthors.addEventListener("click", () => {
-    const authorId = linkPostAuthor.dataset.authorid;
-    const authorUrl = `/users/${authorId}`;
-    window.location.href = authorUrl;
+  linkPostAuthors.forEach((author) => {
+    author.addEventListener("click", () => {
+      const authorId = author.dataset.authorid;
+      const authorUrl = `/users/${authorId}`;
+      window.location.href = authorUrl;
+    });
   });
 
-  linkPostChannels.addEventListener("click", () => {
-    const channelId = linkPostChannel.dataset.channelid;
-    const channelUrl = `/channels/${channelId}`;
-    window.location.href = channelUrl;
+  linkPostChannels.forEach((channel) => {
+    channel.addEventListener("click", () => {
+      const channelId = channel.dataset.channelid;
+      const channelUrl = `/channels/${channelId}`;
+      window.location.href = channelUrl;
+    });
   });
 });

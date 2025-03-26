5e8e373b581efaa01277b15cd0ab6d91ac0b1eb1
Gary Norman <gary.norman.th@gmail.com>
2025-03-26 22:35:39 +0000

    maint: modified post-post, -author, and -channel links to utilise NavigateTo* functions

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,3 +1,5 @@
+import { navigateToChannel, navigateToPost, navigateToAuthor } from "./main.js";
+
 const linkPostPosts = document.querySelectorAll('[id^="link-post-post"]');
 const linkPostAuthors = document.querySelectorAll('[id^="link-post-author"]');
 const linkPostChannels = document.querySelectorAll('[id^="link-post-channel"]');
@@ -6,26 +8,23 @@ const linkPostChannels = document.querySelectorAll('[id^="link-post-channel"]');
 
 document.addEventListener("DOMContentLoaded", () => {
   linkPostPosts.forEach((post) => {
-    post.addEventListener("click", () => {
-      const postId = post.dataset.postid;
-      const postUrl = `/posts/${postId}`;
-      window.location.href = postUrl;
+    post.addEventListener("click", (e) => {
+      e.preventDefault();
+      navigateToPost(post);
     });
   });
 
   linkPostAuthors.forEach((author) => {
-    author.addEventListener("click", () => {
-      const authorId = author.dataset.authorid;
-      const authorUrl = `/users/${authorId}`;
-      window.location.href = authorUrl;
+    author.addEventListener("click", (e) => {
+      e.preventDefault();
+      navigateToAuthor(author);
     });
   });
 
   linkPostChannels.forEach((channel) => {
-    channel.addEventListener("click", () => {
-      const channelId = channel.dataset.channelid;
-      const channelUrl = `/channels/${channelId}`;
-      window.location.href = channelUrl;
+    channel.addEventListener("click", (e) => {
+      e.preventDefault();
+      navigateToChannel(channel);
     });
   });
 });

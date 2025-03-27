70eb7af424668b867eadb49bd7c53be5cfc954e8
Gary Norman <gary.norman.th@gmail.com>
2025-03-27 00:12:20 +0000

    maint: removed post/user functions from handlers.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html/template"
 	"io"
 	"log"
 	"math/rand/v2"
@@ -486,144 +485,8 @@ func (app *app) getCommentsLikesAndDislikes(comments []models.Comment) []models.
 }
 
 // SECTION ------- user handlers ----------
-func (app *app) editUserDetails(w http.ResponseWriter, r *http.Request) {
-	user, ok := getUserFromContext(r.Context())
-	if !ok {
-		log.Printf(ErrorMsgs().KeyValuePair, "user not found in context", "editUserDetails")
-		return
-	}
-
-	err := r.ParseMultipartForm(10 << 20)
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		log.Printf(ErrorMsgs().Parse, "editUserDetails", err)
-		return
-	}
-	currentAvatar := user.Avatar
-	fmt.Printf("currentAvatar: %v", currentAvatar)
-	user.Avatar = GetFileName(r, "file-drop", "editUserDetails", "user")
-	if currentAvatar != "noimage" {
-		user.Avatar = currentAvatar
-	}
-	currentDescription := r.FormValue("bio")
-	if currentDescription != "" {
-		user.Description = currentDescription
-	}
-	currentName := r.FormValue("name")
-	if currentName != "" {
-		user.Username = currentName
-	}
-	editErr := app.users.Edit(user)
-	if editErr != nil {
-		log.Printf(ErrorMsgs().Edit, user.Username, "EditUserDetails", editErr)
-	}
-	if err := app.cookies.CreateCookies(w, user); err != nil {
-		log.Printf(ErrorMsgs().KeyValuePair, "error creating cookies", err)
-	}
-	http.Redirect(w, r, "/", http.StatusFound)
-}
 
 // SECTION ------- post handlers ----------
-func (app *app) createPost(w http.ResponseWriter, r *http.Request) {
-	tpl, parseErr := template.ParseFiles("./assets/templates/posts.create.html")
-	if parseErr != nil {
-		http.Error(w, parseErr.Error(), 500)
-		log.Printf(ErrorMsgs().Parse, "./assets/templates/posts.create.html", "createPost", parseErr)
-		return
-	}
-
-	execErr := tpl.Execute(w, nil)
-	if execErr != nil {
-		log.Printf(ErrorMsgs().Execute, execErr)
-		return
-	}
-}
-
-func (app *app) storePost(w http.ResponseWriter, r *http.Request) {
-	user, ok := getUserFromContext(r.Context())
-	if !ok {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	parseErr := r.ParseMultipartForm(10 << 20)
-	if parseErr != nil {
-		http.Error(w, parseErr.Error(), 400)
-		log.Printf(ErrorMsgs().Parse, "storePost", parseErr)
-		return
-	}
-	channels := r.Form["channel[]"] // Retrieve the slice of checked channel IDs
-	// get channel name
-
-	// SECTION getting channel data (for reverting to single channel post
-	//selectionJSON := r.PostForm.Get("channel")
-	//if selectionJSON == "" {
-	//	http.Error(w, "No selection provided", http.StatusBadRequest)
-	//	return
-	//}
-	//var channelData models.ChannelData
-	//if err := json.Unmarshal([]byte(selectionJSON), &channelData); err != nil {
-	//	log.Printf(ErrorMsgs().Unmarshal, selectionJSON, err)
-	//	http.Error(w, "Invalid selection format", http.StatusBadRequest)
-	//	return
-	//}
-	//fmt.Printf(ErrorMsgs().KeyValuePair, "channelName", channelData.ChannelName)
-	//fmt.Printf(ErrorMsgs().KeyValuePair, "channelID", channelData.ChannelID)
-	fmt.Printf(ErrorMsgs().KeyValuePair, "commentable", r.PostForm.Get("commentable"))
-
-	createPostData := models.Post{
-		Title:         r.PostForm.Get("title"),
-		Content:       r.PostForm.Get("content"),
-		Images:        "",
-		Author:        user.Username,
-		AuthorID:      user.ID,
-		AuthorAvatar:  user.Avatar,
-		IsCommentable: false,
-		IsFlagged:     false,
-	}
-	fmt.Printf(ErrorMsgs().KeyValuePair, "authorAvatar", createPostData.AuthorAvatar)
-	if r.PostForm.Get("commentable") == "on" {
-		createPostData.IsCommentable = true
-	}
-	createPostData.Images = GetFileName(r, "file-drop", "storePost", "post")
-	/*createPostData.ChannelName = channelData.ChannelName
-	createPostData.ChannelID, _ = strconv.Atoi(channelData.ChannelID)*/
-
-	postID, insertErr := app.posts.Insert(
-		createPostData.Title,
-		createPostData.Content,
-		createPostData.Images,
-		createPostData.Author,
-		createPostData.AuthorAvatar,
-		createPostData.AuthorID,
-		createPostData.IsCommentable,
-		createPostData.IsFlagged,
-	)
-
-	if insertErr != nil {
-		log.Printf(ErrorMsgs().Post, insertErr)
-		http.Error(w, insertErr.Error(), 500)
-		return
-	}
-
-	for c := range channels {
-		channelID, convErr := strconv.Atoi(channels[c])
-		if convErr != nil {
-			log.Printf(ErrorMsgs().Convert, channels[c], "StorePost > GetChannelID", convErr)
-			log.Printf("Unable to convert %v to integer\n", channels[c])
-			continue
-		}
-		postToChannelErr := app.channels.AddPostToChannel(channelID, postID)
-		if postToChannelErr != nil {
-			log.Printf(ErrorMsgs().KeyValuePair, "channelID", channelID)
-			log.Printf(ErrorMsgs().KeyValuePair, "postID", postID)
-			log.Printf(ErrorMsgs().KeyValuePair, "postToChannelErr", postToChannelErr)
-			http.Error(w, postToChannelErr.Error(), 500)
-			return
-		}
-	}
-
-	http.Redirect(w, r, "/", http.StatusFound)
-}
 
 func (app *app) storeReaction(w http.ResponseWriter, r *http.Request) {
 	// log.Printf("using storeReaction()")

72e3e7987bb3fe697a1354717c7e1009c3338067
kornal <kamilornal@gmail.com>
2025-04-12 20:28:15 +0100

    refactor: posts.js refactored to use as function to be called on DOMLoaded and HTMLInjected events

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -4,28 +4,30 @@ const linkPostPosts = document.querySelectorAll('[id^="link-post-post"]');
 const linkPostAuthors = document.querySelectorAll('[id^="link-post-author"]');
 const linkPostChannels = document.querySelectorAll('[id^="link-post-channel"]');
 
-document.addEventListener("DOMContentLoaded", () => {
-  linkPostPosts.forEach((post) => {
-    post.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log("clicked post");
-      navigateToPost(post);
+// INFO was a DOMContentLoaded function
+function listenToNavigationLinks()  {
+    linkPostPosts.forEach((post) => {
+      post.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("clicked post");
+        navigateToPost(post);
+      });
     });
-  });
 
-  linkPostAuthors.forEach((author) => {
-    author.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log("clicked author");
-      navigateToAuthor(author);
+    linkPostAuthors.forEach((author) => {
+      author.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("clicked author");
+        navigateToAuthor(author);
+      });
     });
-  });
 
-  linkPostChannels.forEach((channel) => {
-    channel.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log("clicked channel");
-      navigateToChannel(channel);
+    linkPostChannels.forEach((channel) => {
+      channel.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("clicked channel");
+        navigateToChannel(channel);
+      });
     });
-  });
-});
+  }
+

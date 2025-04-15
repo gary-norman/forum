34ba6b91b75b77202915b32bd8253172505c6944
Gary Norman <gary.norman.th@gmail.com>
2025-04-15 18:11:10 +0100

    refactor: abstracted linkPost* functions to a single unified function linkPosts

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,33 +1,49 @@
-import { navigateToChannel, navigateToPost, navigateToAuthor } from "./main.js";
+import {
+  navigateToPage,
+  navigateToChannel,
+  navigateToPost,
+  navigateToUser,
+} from "./main.js";
 
+const linkPosts = document.querySelectorAll('[id^="link-post-"]');
 const linkPostPosts = document.querySelectorAll('[id^="link-post-post"]');
 const linkPostAuthors = document.querySelectorAll('[id^="link-post-author"]');
 const linkPostChannels = document.querySelectorAll('[id^="link-post-channel"]');
 
 // INFO was a DOMContentLoaded function
-export function listenToNavigationLinks()  {
-    linkPostPosts.forEach((post) => {
-      post.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log("clicked post");
-        navigateToPost(post);
-      });
+export function listenToNavigationLinks() {
+  linkPosts.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const fullID = e.target.id;
+      const prefix = "link-post-";
+      const entity = fullID.slice(prefix.length);
+      console.log("clicked ", entity);
+      navigateToPage(entity);
     });
+  });
 
-    linkPostAuthors.forEach((author) => {
-      author.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log("clicked author");
-        navigateToAuthor(author);
-      });
-    });
-
-    linkPostChannels.forEach((channel) => {
-      channel.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log("clicked channel");
-        navigateToChannel(channel);
-      });
-    });
-  }
-
+  // linkPostPosts.forEach((post) => {
+  //   post.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     console.log("clicked post");
+  //     navigateToPost(post);
+  //   });
+  // });
+  //
+  // linkPostAuthors.forEach((author) => {
+  //   author.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     console.log("clicked author");
+  //     navigateToUser(author);
+  //   });
+  // });
+  //
+  // linkPostChannels.forEach((channel) => {
+  //   channel.addEventListener("click", (e) => {
+  //     e.preventDefault();
+  //     console.log("clicked channel");
+  //     navigateToChannel(channel);
+  //   });
+  // });
+}

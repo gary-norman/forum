b35f7dc645f0a6fe528f027468f4b693c7182f66
kornal <kamilornal@gmail.com>
2025-04-21 16:44:49 +0100

    maint: added and modified the getUserPosts function, which now correctly retrieves posts of channels a user follows (of all if follows none, or not logged in)

--- cmd/web/home_handlers.go
+++ cmd/web/home_handlers.go
@@ -1,88 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gary-norman/forum/internal/models"
 	"log"
-	"net/http"
-	"time"
 )
-
-// function filters all posts to find posts belonging to the currently logged in user and reners the page with injected home-page template
-func (app *app) navigateHome(w http.ResponseWriter, r *http.Request) {
-	var userPosts []models.Post
-
-	start := time.Now()
-	// SECTION --- posts and comments ---
-	// var userLoggedIn bool
-	userLoggedIn := true
-
-	allPosts, err := app.posts.All()
-	if err != nil {
-		log.Printf(ErrorMsgs().KeyValuePair, "Error fetching all posts", err)
-	}
-	// Retrieve total likes and dislikes for each post
-	allPosts = app.getPostsLikesAndDislikes(allPosts)
-
-	for p := range allPosts {
-		models.UpdateTimeSince(&allPosts[p])
-	}
-
-	allPosts, err = app.getPostsComments(allPosts)
-	if err != nil {
-		log.Printf(ErrorMsgs().NotFound, "allPosts comments", "getHome", err)
-	}
-
-	for p := range allPosts {
-		channelIDs, err := app.channels.GetChannelIdFromPost(allPosts[p].ID)
-		if err != nil {
-			log.Printf(ErrorMsgs().KeyValuePair, "getHome > channelID", err)
-		}
-		if len(allPosts) > 0 && len(channelIDs) > 0 {
-			allPosts[p].ChannelID = channelIDs[0]
-		} else {
-			fmt.Printf(ErrorMsgs().KeyValuePair, "error fetching posts", "no posts or channel IDs found")
-		}
-	}
-
-	currentUser, ok := getUserFromContext(r.Context())
-	if !ok {
-		userLoggedIn = false
-	}
-
-	if userLoggedIn {
-		userPosts = app.getUserPosts(currentUser, allPosts)
-	} else {
-		userPosts = allPosts
-	}
-
-	data := models.ChannelPage{
-		CurrentUser: currentUser,
-		Instance:    "home-page",
-		Posts:       userPosts,
-		ImagePaths:  app.paths,
-	}
-
-	renderPageData(w, data)
-	log.Printf(ErrorMsgs().KeyValuePair, "Home page rendered in:", time.Since(start))
-}
 
 // getUserPosts returns a slice of Posts that belong to channels the user follows. If no user is logged in, it returns all posts
 func (app *app) getUserPosts(user *models.User, allPosts []models.Post) []models.Post {
 	if user == nil {
 		return allPosts
 	}
+	var ownedChannels, joinedChannels, ownedAndJoinedChannels []models.Channel
 	var postsInUserChannels []models.Post
+	log.Printf(ErrorMsgs().KeyValuePair, "Home page - user.ID", user.ID)
 
-	userChannels, memberErr := app.memberships.UserMemberships(user.ID)
+	memberships, memberErr := app.memberships.UserMemberships(user.ID)
 	if memberErr != nil {
-		log.Printf(ErrorMsgs().KeyValuePair, "Error getting user memberships in response func: ", memberErr)
+		log.Printf(ErrorMsgs().KeyValuePair, "getHome > UserMemberships", memberErr)
 	}
+	ownedChannels, ownedErr := app.channels.OwnedOrJoinedByCurrentUser(user.ID, "OwnerID")
+	if ownedErr != nil {
+		log.Printf(ErrorMsgs().Query, "user owned channels", ownedErr)
+	}
+	joinedChannels, joinedErr := app.JoinedByCurrentUser(memberships)
+	if joinedErr != nil {
+		log.Printf(ErrorMsgs().Query, "user joined channels", joinedErr)
+	}
+	ownedAndJoinedChannels = append(ownedChannels, joinedChannels...)
+
+	if len(ownedAndJoinedChannels) == 0 {
+		return allPosts
+	}
+
+	log.Printf(ErrorMsgs().KeyValuePair, "Home page - userChannels", ownedAndJoinedChannels)
 
 	// Create a set of user's channel IDs for efficient lookup
 	userChannelIDSet := make(map[int]bool)
-	for _, membership := range userChannels {
-		userChannelIDSet[membership.ChannelID] = true
+	for _, membership := range ownedAndJoinedChannels {
+		userChannelIDSet[membership.ID] = true
 	}
 
 	// Iterate over all the posts and check if their ChannelID is in the user's channel set

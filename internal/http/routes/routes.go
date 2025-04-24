bad798450bc85de9bffa69a3311bc54ecd31aef5
Gary Norman <gary.norman.th@gmail.com>
2025-04-24 02:33:43 +0100

    refactor: revamped web server, changed related functions to routes and middleware

--- internal/http/routes/routes.go
+++ internal/http/routes/routes.go
@@ -1,50 +1,41 @@
-package handlers
+package routes
 
 import (
 	"net/http"
 
+	"github.com/gary-norman/forum/internal/app"
+	// "github.com/gary-norman/forum/internal/http/handlers"
 	mw "github.com/gary-norman/forum/internal/http/middleware"
 )
 
-func (r *RouteHandler) Routes() http.Handler {
-	s := r
+func NewRouter(app *app.App) http.Handler {
 	mux := http.NewServeMux()
-	cssServer := http.FileServer(http.Dir("./assets/css"))
-	mux.Handle("/assets/css/", http.StripPrefix("/assets/css", cssServer))
-	jsServer := http.FileServer(http.Dir("./assets/js"))
-	mux.Handle("/assets/js/", http.StripPrefix("/assets/js", jsServer))
-	iconServer := http.FileServer(http.Dir("./assets/icons"))
-	mux.Handle("/assets/icons/", http.StripPrefix("/assets/icons", iconServer))
-	imageServer := http.FileServer(http.Dir("./assets/images"))
-	mux.Handle("/assets/images/", http.StripPrefix("/assets/images", imageServer))
-	dbImageServer := http.FileServer(http.Dir("./db/userdata/images"))
-	mux.Handle("/db/userdata/images/", http.StripPrefix("/db/userdata/images", dbImageServer))
-	fontServer := http.FileServer(http.Dir("./assets/fonts"))
-	mux.Handle("/assets/fonts/", http.StripPrefix("/assets/fonts", fontServer))
-	cursorServer := http.FileServer(http.Dir("./assets/cursors"))
-	mux.Handle("/assets/cursors/", http.StripPrefix("/assets/cursors", cursorServer))
-	userDataServer := http.FileServer(http.Dir(" e./db/userdata"))
-	mux.Handle("/db/userdata/", http.StripPrefix("/db/userdata", userDataServer))
-	mux.HandleFunc("POST /register", s.Auth.Register)
-	mux.HandleFunc("POST /login", s.Auth.Login)
-	mux.HandleFunc("POST /logout", s.Auth.Logout)
-	mux.HandleFunc("POST /protected", s.Auth.Protected)
-	mux.Handle("GET /", mw.WithUser(http.HandlerFunc(s.Home.GetHome), s.App))
-	mux.Handle("GET /channels/{channelId}", mw.WithUser(http.HandlerFunc(s.Channel.GetThisChannel), s.App))
-	mux.HandleFunc("GET /posts/create", s.Post.CreatePost)
-	mux.HandleFunc("GET /search", s.Search.Search)
-	mux.Handle("GET /posts/{postId}", mw.WithUser(http.HandlerFunc(s.Post.GetThisPost), s.App))
-	mux.Handle("GET /users/{userId}", mw.WithUser(http.HandlerFunc(s.User.GetThisUser), s.App))
-	mux.Handle("POST /posts/create", mw.WithUser(http.HandlerFunc(s.Post.StorePost), s.App))
-	mux.Handle("POST /channels/create", mw.WithUser(http.HandlerFunc(s.Channel.StoreChannel), s.App))
-	mux.HandleFunc("/posts/create", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	})
-	mux.Handle("POST /store-reaction", mw.WithUser(http.HandlerFunc(s.Reaction.StoreReaction), s.App))
-	mux.Handle("POST /edituser", mw.WithUser(http.HandlerFunc(s.User.EditUserDetails), s.App))
-	mux.Handle("POST /channels/join", mw.WithUser(http.HandlerFunc(s.Channel.StoreMembership), s.App))
-	mux.Handle("POST /channels/add-rules/{channelId}", mw.WithUser(http.HandlerFunc(s.Channel.CreateAndInsertRule), s.App))
-	mux.Handle("POST /store-comment", mw.WithUser(http.HandlerFunc(s.Comment.StoreComment), s.App))
+	r := NewRouteHandler(app)
+
+	// Static
+	// handlers.MuxHandler(mux, "assets")
+	// handlers.MuxHandler(mux, "db")
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	mux.Handle("/db/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./db"))))
+
+	// Core routes
+	mux.HandleFunc("POST /register", r.Auth.Register)
+	mux.HandleFunc("POST /login", r.Auth.Login)
+	mux.HandleFunc("POST /logout", r.Auth.Logout)
+	mux.HandleFunc("POST /protected", r.Auth.Protected)
+	mux.Handle("/", mw.WithUser(http.HandlerFunc(r.Home.GetHome), r.App))
+	mux.Handle("GET /channels/{channelId}", mw.WithUser(http.HandlerFunc(r.Channel.GetThisChannel), r.App))
+	mux.HandleFunc("GET /posts/create", r.Post.CreatePost)
+	mux.HandleFunc("GET /search", r.Search.Search)
+	mux.Handle("GET /posts/{postId}", mw.WithUser(http.HandlerFunc(r.Post.GetThisPost), r.App))
+	mux.Handle("GET /users/{userId}", mw.WithUser(http.HandlerFunc(r.User.GetThisUser), r.App))
+	mux.Handle("POST /posts/create", mw.WithUser(http.HandlerFunc(r.Post.StorePost), r.App))
+	mux.Handle("POST /channels/create", mw.WithUser(http.HandlerFunc(r.Channel.StoreChannel), r.App))
+	mux.Handle("POST /store-reaction", mw.WithUser(http.HandlerFunc(r.Reaction.StoreReaction), r.App))
+	mux.Handle("POST /edituser", mw.WithUser(http.HandlerFunc(r.User.EditUserDetails), r.App))
+	mux.Handle("POST /channels/join", mw.WithUser(http.HandlerFunc(r.Channel.StoreMembership), r.App))
+	mux.Handle("POST /channels/add-rules/{channelId}", mw.WithUser(http.HandlerFunc(r.Channel.CreateAndInsertRule), r.App))
+	mux.Handle("POST /store-comment", mw.WithUser(http.HandlerFunc(r.Comment.StoreComment), r.App))
 
 	return mux
 }

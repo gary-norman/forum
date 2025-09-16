0cc7f917f061585fe3eceee5d0948b679eea50ff
Gary Norman <gary.norman.th@gmail.com>
2025-09-16 22:15:45 +0100

    maint: passed error to RenderErrorPage, passed 500 error

--- internal/view/notfoundmodel.go
+++ internal/view/notfoundmodel.go
@@ -14,7 +14,7 @@ type NotFound interface {
 	GetInstance() string
 }
 
-func RenderErrorPage[T NotFound](w http.ResponseWriter, data T) {
+func RenderErrorPage[T NotFound](w http.ResponseWriter, data T, error error) {
 	var renderedPage bytes.Buffer
 	instance := data.GetInstance()
 	HTMLstr := models.ToHTMLVar(instance)
@@ -32,8 +32,12 @@ func RenderErrorPage[T NotFound](w http.ResponseWriter, data T) {
 		HTMLstr: renderedPage.String(),
 	}
 
+	w.WriteHeader(http.StatusInternalServerError)
 	// Write the response as JSON
 	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, `{"error": "error encoding JSON"}`, http.StatusInternalServerError)
+		errorStr := fmt.Sprintf("Error encoding JSON for %v: %v", instance, err)
+		http.Error(w, errorStr, http.StatusInternalServerError)
 	}
+
+	log.Println(error)
 }

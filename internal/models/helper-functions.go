2d4bf834b5f65d97b6b302742dbcf443cc8f54be
Gary Norman <gary.norman.th@gmail.com>
2025-09-17 13:02:59 +0100

    maint: commented all functions

--- internal/models/helper-functions.go
+++ internal/models/helper-functions.go
@@ -7,21 +7,27 @@ import (
 	"time"
 )
 
+// TimeUpdatable is an interface for types that can update their TimeSince field.
 type TimeUpdatable interface {
 	UpdateTimeSince()
 }
+
+// Reactable is an interface for types that can be reacted to with likes and dislikes.
 type Reactable interface {
 	React(likes, dislikes int)
 }
 
+// UpdateTimeSince updates the TimeSince field of any struct that implements the TimeUpdatable interface.
 func UpdateTimeSince(t TimeUpdatable) {
 	t.UpdateTimeSince()
 }
 
+// React updates the likes and dislikes of any struct that implements the Reactable interface.
 func React(r Reactable, likes, dislikes int) {
 	r.React(likes, dislikes)
 }
 
+// getTimeSince returns a human-readable string representing the time elapsed since the provided creation time.
 func getTimeSince(created time.Time) string {
 	now := time.Now()
 	hours := now.Sub(created).Hours()
@@ -38,6 +44,7 @@ func getTimeSince(created time.Time) string {
 	return timeSince
 }
 
+// GetIntFromPathValue parses a string value from the URL path and converts it to int64, returning an error if conversion fails.
 func GetIntFromPathValue(value string) (int64, error) {
 	intValue, err := strconv.ParseInt(value, 10, 64)
 	if err != nil {

ded8863b595982cd629e18c51f610243b27ac013
Gary Norman <gary.norman.th@gmail.com>
2025-09-18 23:44:03 +0100

    maint: improved variable naming and returned error

--- internal/view/pagemodel.go
+++ internal/view/pagemodel.go
@@ -17,10 +17,9 @@ type PageModel interface {
 func RenderPageData[T PageModel](w http.ResponseWriter, data T) {
 	var renderedPage bytes.Buffer
 	instance := data.GetInstance()
-	HTMLstr := models.ToHTMLVar(instance)
+	htmlkey := models.ToHTMLVar(instance)
 
-	err := Template.ExecuteTemplate(&renderedPage, instance, data)
-	if err != nil {
+	if err := Template.ExecuteTemplate(&renderedPage, instance, data); err != nil {
 		errorStr := fmt.Sprintf("Error rendering %v: %v", instance, err)
 		log.Println(errorStr)
 		http.Error(w, errorStr, http.StatusInternalServerError)
@@ -29,11 +28,12 @@ func RenderPageData[T PageModel](w http.ResponseWriter, data T) {
 
 	// Send the pre-rendered HTML as JSON
 	response := map[string]string{
-		HTMLstr: renderedPage.String(),
+		htmlkey: renderedPage.String(),
 	}
 
 	// Write the response as JSON
 	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, `{"error": "error encoding JSON"}`, http.StatusInternalServerError)
+		errorStr := fmt.Sprintf("Error encoding JSON for %v: %v", instance, err)
+		http.Error(w, errorStr, http.StatusInternalServerError)
 	}
 }

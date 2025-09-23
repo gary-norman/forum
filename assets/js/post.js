c571bd06b7e228559c8d3e20f6ce3eb21b62d7f0
Gary Norman <gary.norman.th@gmail.com>
2025-09-23 17:56:26 +0100

    fix: catched all input errrors
    but failing as cant find image when not uploaded

--- assets/js/post.js
+++ assets/js/post.js
@@ -29,88 +29,74 @@ import {
 //     });
 //   });
 
-export function listenToPostLinks() {
-  const notifierPost = document.querySelector("#post-popover-title");
-  const createPostForm = document.querySelector("#form-create-post");
-  if (createPostForm) {
-    const btnSelectChannels = createPostForm.querySelector(".dropdown-toggle");
-    const inputTitle = createPostForm.querySelector('input[name="title"]');
-    const inputContent = createPostForm.querySelector(
-      'textarea[name="content"]',
-    );
-    createPostForm.addEventListener("submit", async function (e) {
-      e.preventDefault();
-      const form = e.target;
-      const formData = new FormData(form); // Collect form data
-      const channels = formData.getAll("post_channel_list"); // get an array of selected channels
+const notifierPost = document.querySelector("#post-popover-title");
+const createPostForm = document.querySelector("#form-create-post");
+if (createPostForm) {
+  const btnSelectChannels = createPostForm.querySelector(".dropdown-toggle");
+  const inputTitle = createPostForm.querySelector('input[name="title"]');
+  const inputContent = createPostForm.querySelector('textarea[name="content"]');
+  createPostForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form); // Collect form data
+    const channels = formData.getAll("post_channel_list"); // get an array of selected channels
+    const title = formData.get("title")?.trim();
+    const content = formData.get("content")?.trim();
 
-      if (channels.length < 2) {
-        showInlineNotification(
-          notifierPost,
-          "",
-          "select at least 2 channels",
-          false,
-          "dummy",
-        );
-        btnSelectChannels.classList.add("active");
+    if (channels.length < 2) {
+      showInlineNotification(
+        notifierPost,
+        "",
+        "select at least 2 channels",
+        false,
+        "dummy",
+      );
+      btnSelectChannels.classList.add("active");
+      return;
+    }
+    if (!title) {
+      showInlineNotification(notifierPost, "", "enter a title", false, "dummy");
+      inputTitle.focus();
+      return;
+    }
+    if (!content) {
+      showInlineNotification(
+        notifierPost,
+        "",
+        "enter some content",
+        false,
+        "dummy",
+      );
+      inputContent.focus();
+      return;
+    }
+
+    try {
+      const response = await fetch("/posts/create", {
+        method: "POST",
+        body: formData,
+        cache: "no-store",
+      });
+
+      const data = await response.json();
+
+      if (!response.ok || data.message === "post creation failed!") {
+        showInlineNotification(notifierPost, "", data.message, false, "dummy");
         return;
       }
-      if (!formData.title) {
-        showInlineNotification(
-          notifierPost,
-          "",
-          "enter a title",
-          false,
-          "dummy",
-        );
-        inputTitle.focus();
-        return;
-      }
-      if (!formData.content) {
-        showInlineNotification(
-          notifierPost,
-          "",
-          "enter some content",
-          false,
-          "dummy",
-        );
-        inputContent.focus();
-        return;
-      }
-
-      try {
-        const response = await fetch("/posts/create", {
-          method: "POST",
-          body: formData,
-          cache: "no-store",
-        });
-
-        const data = await response.json();
-
-        if (!response.ok || data.message === "post creation failed!") {
-          showInlineNotification(
-            notifierPost,
-            "",
-            data.message,
-            false,
-            "dummy",
-          );
-          return;
-        }
-        showInlineNotification(notifierPost, "", data.message, true, "dummy");
-        setTimeout(
-          function () {
-            popoverTargetElement = this;
-            popoverTargetAction = "hide";
-          }.bind(this),
-          2000,
-        );
-      } catch (error) {
-        console.custom.error("Error during post creation:", error);
-        showMainNotification("An error occurred during post creation.");
-      }
-    });
-  }
+      showInlineNotification(notifierPost, "", data.message, true, "dummy");
+      setTimeout(
+        function () {
+          popoverTargetElement = this;
+          popoverTargetAction = "hide";
+        }.bind(this),
+        2000,
+      );
+    } catch (error) {
+      console.custom.error("Error during post creation:", error);
+      showMainNotification("An error occurred during post creation.");
+    }
+  });
 }
 
 // post channel selection dropdown

f3ec25040fbbf14e1266a5aa4f8b2b53baaad3c4
Gary Norman <gary.norman.th@gmail.com>
2025-09-23 02:18:00 +0100

    refactor: split GetHome into RenderIndex and GetHome
    RenderIndex renders the main page
    GetHome injects the home-page data

--- internal/http/handlers/home-handlers.go
+++ internal/http/handlers/home-handlers.go
@@ -25,12 +25,19 @@ type HomeHandler struct {
 
 var TemplateData models.TemplateData
 
-func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
-	if r.PathValue("invalidString") != "" {
-		view.RenderErrorPage(w, models.NotFoundLocation("home"), 400, models.NotFoundError(r.PathValue("invalidString"), "GetHome", fmt.Errorf("the user entered %v after /", r.PathValue("invalidString"))))
-	}
+func (h *HomeHandler) RenderIndex(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	var userPosts []models.Post
+	// fmt.Println(r.PathValue("invalidString"))
+	// stringCheck := []string{" ", "favicon.ico"}
+	// if r.PathValue("invalidString") != "" {
+	// 	for _, valid := range stringCheck {
+	// 		if r.PathValue("invalidString") != valid {
+	// 			view.RenderErrorPage(w, models.NotFoundLocation("home"), 400, models.NotFoundError(r.PathValue("invalidString"), "RenderIndex", fmt.Errorf("the user entered %v after /", r.PathValue("invalidString"))))
+	// 			return
+	// 		}
+	// 	}
+	// }
+
 	userLoggedIn := true
 	userID := models.ZeroUUIDField()
 	// SECTION --- posts and comments ---
@@ -134,7 +141,6 @@ func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
 
 	if userLoggedIn {
 		userID = currentUser.ID
-		userPosts = h.Post.GetUserPosts(currentUser, allPosts)
 		// attach following/follower numbers to currently logged-in user
 		currentUser.Followers, currentUser.Following, err = h.App.Loyalty.CountUsers(currentUser.ID)
 		if err != nil {
@@ -170,9 +176,6 @@ func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
 				ownedAndJoinedChannels = append(ownedAndJoinedChannels, channel)
 			}
 		}
-
-	} else {
-		userPosts = allPosts
 	}
 
 	// SECTION -- template ---
@@ -182,9 +185,6 @@ func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
 		AllUsers:    allUsers,
 		RandomUser:  randomUser,
 		CurrentUser: currentUser,
-		// ---------- posts ----------
-		Posts:     allPosts,
-		UserPosts: userPosts,
 		// ---------- channels ----------
 		AllChannels:            allChannels,
 		OwnedChannels:          ownedChannels,
@@ -214,7 +214,163 @@ func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
 		log.Printf(ErrorMsgs.Execute, execErr)
 		return
 	}
-	log.Printf(ErrorMsgs().KeyValuePair, "GetHome Render time:", time.Since(start))
+	log.Printf(ErrorMsgs.KeyValuePair, "RenderIndex Render time", time.Since(start))
+}
 
-	// view.RenderPageData(w, data)
+func (h *HomeHandler) GetHome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	start := time.Now()
+	var userPosts []models.Post
+	userLoggedIn := true
+	userID := models.ZeroUUIDField()
+
+	// SECTION --- user ---
+	allUsers, allUsersErr := h.App.Users.All()
+	if allUsersErr != nil {
+		log.Printf(ErrorMsgs.Query, "getHome> users > All", allUsersErr)
+	}
+	for u := range allUsers {
+		models.UpdateTimeSince(&allUsers[u])
+	}
+
+	// attach following/follower numbers to each user
+	for u := range allUsers {
+		allUsers[u].Followers, allUsers[u].Following, allUsersErr = h.App.Loyalty.CountUsers(allUsers[u].ID)
+		if allUsersErr != nil {
+			log.Printf(ErrorMsgs.Query, "getHome> users > All > loyalty", allUsersErr)
+		}
+	}
+
+	randomUser := GetRandomUser(allUsers)
+	currentUser, ok := mw.GetUserFromContext(r.Context())
+	if !ok {
+		userLoggedIn = false
+	}
+
+	var currentUserErr error
+	// attach following/follower numbers to the random user
+	randomUser.Followers, randomUser.Following, currentUserErr = h.App.Loyalty.CountUsers(randomUser.ID)
+	if currentUserErr != nil {
+		log.Printf(ErrorMsgs.Query, "getHome> users > All", allUsersErr)
+	}
+
+	// SECTION --- posts and comments ---
+	allPosts, err := h.App.Posts.All()
+	if err != nil {
+		log.Printf(ErrorMsgs.KeyValuePair, "Error fetching all posts", err)
+	}
+	// Retrieve total likes and dislikes for each post
+	allPosts = h.Reaction.GetPostsLikesAndDislikes(allPosts)
+
+	// Retrieve last reaction time for posts
+	allPosts, err = h.Reaction.getLastReactionTimeForPosts(allPosts)
+	if err != nil {
+		log.Printf(ErrorMsgs.KeyValuePair, "Error getting last reaction time for allPosts", err)
+	}
+
+	for p := range allPosts {
+		models.UpdateTimeSince(&allPosts[p])
+	}
+	allPosts, err = h.Comment.GetPostsComments(allPosts)
+	if err != nil {
+		log.Printf(ErrorMsgs.NotFound, "allPosts comments", "getHome", err)
+	}
+
+	for p := range allPosts {
+		channelIDs, err := h.App.Channels.GetChannelIDFromPost(allPosts[p].ID)
+		if err != nil {
+			log.Printf(ErrorMsgs.KeyValuePair, "getHome > channelID", err)
+		}
+		if len(allPosts) > 0 && len(channelIDs) > 0 {
+			allPosts[p].ChannelID = channelIDs[0]
+		} else {
+			fetchErr := fmt.Sprintf("post ID: %v does not belong to any channel", allPosts[p].ID)
+			fmt.Printf(ErrorMsgs.KeyValuePair, "error fetching posts", fetchErr)
+		}
+	}
+
+	// SECTION --- channels --
+	allChannels, err := h.App.Channels.All()
+	if err != nil {
+		log.Printf(ErrorMsgs.Query, "channels.All", err)
+	}
+	for c := range allChannels {
+		models.UpdateTimeSince(&allChannels[c])
+	}
+
+	for p := range allPosts {
+		for _, channel := range allChannels {
+			if channel.ID == allPosts[p].ChannelID {
+				allPosts[p].ChannelName = channel.Name
+			}
+		}
+	}
+
+	ownedChannels := make([]models.Channel, 0)
+	joinedChannels := make([]models.Channel, 0)
+	ownedAndJoinedChannels := make([]models.Channel, 0)
+	channelMap := make(map[int64]bool)
+
+	if userLoggedIn {
+		userID = currentUser.ID
+		userPosts = h.Post.GetUserPosts(currentUser, allPosts)
+		// attach following/follower numbers to currently logged-in user
+		currentUser.Followers, currentUser.Following, err = h.App.Loyalty.CountUsers(currentUser.ID)
+		if err != nil {
+			fmt.Printf(ErrorMsgs.KeyValuePair, "getHome > currentUser loyalty", err)
+		}
+		// get owned and joined channels of current user
+		memberships, memberErr := h.App.Memberships.UserMemberships(currentUser.ID)
+		if memberErr != nil {
+			log.Printf(ErrorMsgs.KeyValuePair, "getHome > UserMemberships", memberErr)
+		}
+		ownedChannels, err = h.App.Channels.OwnedOrJoinedByCurrentUser(currentUser.ID)
+		if err != nil {
+			log.Printf(ErrorMsgs.Query, "GetHome > user owned channels", err)
+		}
+		joinedChannels, err = h.Channel.JoinedByCurrentUser(memberships)
+		if err != nil {
+			log.Printf(ErrorMsgs.Query, "user joined channels", err)
+		}
+
+		// ownedAndJoinedChannels = append(ownedChannels, joinedChannels...)
+		// Add owned channels
+		for _, channel := range ownedChannels {
+			if !channelMap[channel.ID] {
+				channelMap[channel.ID] = true
+				ownedAndJoinedChannels = append(ownedAndJoinedChannels, channel)
+			}
+		}
+
+		// Add joined channels
+		for _, channel := range joinedChannels {
+			if !channelMap[channel.ID] {
+				channelMap[channel.ID] = true
+				ownedAndJoinedChannels = append(ownedAndJoinedChannels, channel)
+			}
+		}
+
+	} else {
+		userPosts = allPosts
+	}
+
+	// SECTION -- template ---
+	data := models.HomePage{
+		// ---------- users ----------
+		UserID:      userID,
+		CurrentUser: currentUser,
+		// ---------- posts ----------
+		AllPosts:  allPosts,
+		UserPosts: userPosts,
+		// ---------- channels ----------
+		OwnedChannels:          ownedChannels,
+		JoinedChannels:         joinedChannels,
+		OwnedAndJoinedChannels: ownedAndJoinedChannels,
+		// ---------- misc ----------
+		Instance:   "home-page",
+		ImagePaths: h.App.Paths,
+	}
+
+	view.RenderPageData(w, data)
+	log.Printf(ErrorMsgs.KeyValuePair, "GetHome Render time", time.Since(start))
 }

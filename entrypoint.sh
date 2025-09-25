48677e4791a2ad6fe084221cc8f66e89ea8048c9
Gary Norman <gary.norman.th@gmail.com>
2025-09-25 19:35:10 +0100

    maint: entrypoint stripped right down to only handle essential build logic.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,72 +1,13 @@
-#!/bin/sh
+#!/busybox/sh
 set -e
 
-# Path to the database
-DB_PATH="/var/lib/db-codex/forum_database.db"
+DB_FILE=/var/lib/db-codex/forum_database.db
 
-# create UserID as uuid
-UserID=$(python3 -c "import uuid; print(uuid.uuid4().hex)")
+# Initialize DB if missing
+if [ ! -f "$DB_FILE" ]; then
+  echo "Initializing database..."
+  /app/codex migrate /migrations/001_schema.sql
+  /app/codex seed
+fi
 
-# Insert the user into the Users table
-sqlite3 "$DB_PATH" <<EOF
-INSERT INTO Users (ID, Username, EmailAddress, Avatar, Banner, Description, Usertype, Created, IsFlagged, SessionToken, CsrfToken, HashedPassword)
-VALUES (
-    x'$UserID',
-    'TheCodexDonkey',
-    'donkey@codex.com',
-    'donkey.png',
-    '',
-    "I'm such a friendly donkey, and I'm here to show you around the wonderful world of Codex. I've already added you to my channel, where you'll find updates, information and any cool stuff I feel like sharing. Welcome to Codex!",
-    0,
-    DateTime('now'),
-    0,
-    '',
-    '',
-    '$2a$14$qK2P4N12utI8c4dPS6AMaueafDJygKtdVHVLgNVq2wJM5MW5xjdVm'
-);
-EOF
-
-# Insert the channel into the Channels table
-sqlite3 "$DB_PATH" <<EOF
-INSERT INTO Channels (OwnerID, Name, Description, Created, Avatar, Banner, Privacy, IsFlagged, IsMuted)
-VALUES (
-    x'$UserID',
-    'WelcomeToCodex',
-    "Welcome to Codex! This channel will guide you through the forum, and give you updates, insights, and generally keep you up to date with everything that's going on.",
-    DateTime('now'),
-    'codex.png',
-    '',
-    0,
-    0,
-    0
-);
-EOF
-
-# Insert the post into the posts table
-sqlite3 "$DB_PATH" <<EOF
-INSERT INTO Posts (Title, Content, Images, Created, Author, AuthorAvatar, AuthorID, IsCommentable, IsFlagged)
-VALUES (
-  'Welcome to Codex!',
-  "We are so glad you could join us! Since you're here, you probably already know this, but just in case, this is a place for us all to share everything we know about coding - our hopes, fears, plans, anxieties... Everything that makes up this world in which we find ourselves. Hopefully, Codex will make it less daunting, and a world we enjoy living in. So, welcome! We're so glad you're here.",
-  'noimage',
-  DateTime('now'),
-  'TheCodexDonkey',
-  'donkey.png',
-  x'$UserID',
-  0,
-  0
-);
-EOF
-
-# Attach the post to the channel
-sqlite3 "$DB_PATH" <<EOF
-INSERT INTO PostChannels (PostID, ChannelID, Created)
-VALUES (
-    1,
-    1,
-    DateTime('now')
-);
-EOF
-
-# Continue with the original command
 exec "$@"
